74d6deab61d5fe59ae54c0c2c4ed8b43262f371e
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2020-12-25 23:06:32 +0530

    react

---
+++ src/components/form.js
@@ -0,0 +1,6 @@
+const Form = () =>{
+    return(
+        <input type="text" placeholder="enter the name"/>
+    )
+}
+export default Form;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+const Cards = () =>{
+    return(
+        <h1>
+            hello
+        </h1>
+    )
+}
+export default Cards;
